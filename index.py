93077bc808280c2eaf0be2708e386f9316bb9670
devsungyeon <sungyeonlee1103@gmail.com>
2021-02-03 21:06:38 +0900

    ing

--- index.py
+++ index.py
@@ -1,12 +1,20 @@
 #!C:\Python39\python.exe
 print("content-type: text/html; charset=utf-8\n")
 print()
-import cgi
-
+import cgi, os
 form = cgi.FieldStorage()
-pageId = form["id"].value
 
-pageId = 'Welcome'
+files = os.listdir('data')
+listStr = ''
+for item in files:
+    listStr = listStr + '<li><a href=index.py?id={name}>{name}</a></li>'.format(name = item)
+
+if 'id' in form:
+    pageId = form["id"].value
+    description = open('data/' + pageId, 'r').read()
+else:
+    pageId = 'Welcome'
+    description = 'Hello, web'
 
 print('''
 <!DOCTYPE html>
@@ -16,19 +24,13 @@ print('''
     <meta charset="utf-8">
 </head>
 <body>
-    <h1><a href="index_no_id.py">WEB</a></h1>
+    <h1><a href="index.py">WEB</a></h1>
     <ol>
-        <li><a href="index.py?id=HTML">HTML</a></li>
-        <li><a href="index.py?id=CSS">CSS</a></li>
-        <li><a href="index.py?id=JavaScript">Javascript</a></li>
+        {listStr}
     </ol>
 
     <h2>{title}</h2>
-    <P>
-        Web is a computer programming system created by Donald E. Knuth as the first implementation of what he called "literate programming": the idea that one could create software as works of literature, by embedding source code inside descriptive text, rather than the reverse (as is common practice in most programming languages), in an order that is convenient for exposition to human readers, rather than in the order demanded by the compiler.
-
-        Web consists of two secondary programs: TANGLE, which produces compilable Pascal code from the source texts, and WEAVE, which produces nicely-formatted, printable documentation using TeX.
-    </P>
+    <P>{desc}</P>
 </body>
 </html>
-'''.format(title = pageId))
+'''.format(title = pageId, desc=description, listStr=listStr))
